54f01f97ca4b86d00287de1e039d456e8713781d
jebadirad <ebadirad@email.arizona.edu>
2016-04-09 12:38:06 -0700

    project initialize

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace InvoiceApplication
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+
+            bundles.Add(new ScriptBundle("~/bundles/InvoiceApplication")
+                .IncludeDirectory("~/Scripts/Controllers", "*.js")
+                .Include("~/Scripts/InvoiceApplication.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/site.css"));
+
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ Scripts/InvoiceApplication.js
@@ -0,0 +1,23 @@
+﻿var InvoiceApplication = angular.module('InvoiceApplication', ['ngRoute']);
+
+InvoiceApplication.controller('LandingPageController', LandingPageController);
+
+var configFunction = function ($routeProvider, $httpProvider, $locationProvider) {
+    $locationProvider.hashPrefix('!').html5Mode(true);
+
+    $routeProvider
+        .when("/routeOne", {
+            templateUrl: 'routesDemo/one'
+        })
+        .when("/routeTwo/:donuts", {
+            templateUrl: function (params) { return '/routesDemo/two?donuts=' + params.donuts;}
+        })
+        .when("/routeThree", {
+            templateUrl: 'routesDemo/three'
+        });
+
+}
+
+configFunction.$inject = ["$routeProvider","$httpProvider", "$locationProvider"];
+
+InvoiceApplication.config(configFunction);
